a3c531b1e4cf530cc6117308f481ef6c28ce89bf
Paulius Putna <up614961@myport.ac.uk>
2015-10-27 16:45:49 +0000

    Spektrix style stuff

--- tasks/pixrem.js
+++ tasks/pixrem.js
@@ -1,19 +1,19 @@
 'use strict';
 module.exports = {
-	options: {
-		rootvalue: '62.5%',
-		replace: true
-	},
-	dist: {
-		src: '<%= assetsBuildFolder %>/css/ie.css',
-		dest: '<%= assetsBuildFolder %>/css/ie.css'
-	}
-    // options: {
-    //     rootvalue: '62.5%',
-    //     replace: true
-    // },
-    // dist: {
-    //     src: '<%= assetsBuildFolder %>/css/spektrix.css',
-    //     dest: '<%= assetsBuildFolder %>/css/spektrix.css'
-    // }
+	// options: {
+	// 	rootvalue: '62.5%',
+	// 	replace: true
+	// },
+	// dist: {
+	// 	src: '<%= assetsBuildFolder %>/css/ie.css',
+	// 	dest: '<%= assetsBuildFolder %>/css/ie.css'
+	// }
+    options: {
+        rootvalue: '62.5%',
+        replace: true
+    },
+    dist: {
+        src: '<%= assetsBuildFolder %>/css/spektrix.css',
+        dest: '<%= assetsBuildFolder %>/css/spektrix.css'
+    }
 };
